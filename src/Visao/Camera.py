0938b1c99f0928131469769668b194053cc26109
Gabriel Dinse <gabriel_dinse@hotmail.com>
2020-11-11 12:02:34 -0300

    Alterou comportamento da camera

--- src/Visao/Camera.py
+++ src/Visao/Camera.py
@@ -4,22 +4,71 @@
 # Made with PyCharm
 
 # Standard Library
+import threading
 
 # Third party modules
 import cv2
 
 # Local application imports
+from Helper import CameraEvents
 
 
 class Camera:
-    def __init__(self, video_source=0, camera_resolution=(640, 480)):
-        self.stream = cv2.VideoCapture(video_source)
+    def __init__(self, source_id=0, camera_resolution=(640, 480)):
+        self.source_id = source_id
         self.width, self.height = camera_resolution
+        self.running = False
+        self.new_frame_condition = threading.Condition()
+
+    def open(self):
+        self.stream = cv2.VideoCapture(self.source_id)
         self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, self.width)
         self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
+        self.running = True
+        self.thread = threading.Thread(target=self._run, args=())
+        self.thread.start()
 
-    def read(self):
-        return self.stream.read()
+    def _run(self):
+        while self.running:
+            grabbed, frame = self.stream.read()
+            with self.new_frame_condition:
+                self.grabbed, self.frame = grabbed, frame
+                self.new_frame_condition.notify_all()
 
     def release(self):
+        self.running = False
+        self.thread.join()
         self.stream.release()
+
+    def create_frames_reader(self):
+        return FramesReader(self)
+
+
+class FramesReader:
+    def __init__(self, camera: Camera):
+        self.camera = camera
+        self.events = CameraEvents()
+        self.can_get_frame = threading.Event()
+
+    def read(self, timeout=1.0):
+        if self.can_get_frame.wait(timeout=timeout):
+            with self.camera.new_frame_condition:
+                self.can_get_frame.clear()
+                return self.grabbed, self.frame
+
+    def start(self):
+        self.thread = threading.Thread(target=self._run, args=())
+        self.thread.start()
+        return self
+
+    def bind(self, **kwargs):
+        self.events.bind(**kwargs)
+
+    def _run(self):
+        while self.camera.running:
+            with self.camera.new_frame_condition:
+                if self.camera.new_frame_condition.wait(timeout=1.0):
+                    self.grabbed = self.camera.grabbed
+                    self.frame = self.camera.frame.copy()
+                    self.can_get_frame.set()
+                    self.events.emit("new_frame")
