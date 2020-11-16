268cd62f4427264254e1ee03b774c614c6aebe82
Felipe Nack <felipe.a.nack@gmail.com>
2020-11-16 15:59:45 -0300

    Atualização PackageCentroid

--- src/Visao/PackageCentroid.py
+++ src/Visao/PackageCentroid.py
@@ -3,27 +3,12 @@ import cv2 as cv
 import numpy as np
 import argparse
 import random as rng
-rng.seed(12345)
-def thresh_callback(val1):
-    threshold1 = val1
-    #threshold2 = val2
-    # Detect edges using Canny
-    canny_output = cv.Canny(src_gray, threshold1, threshold1*2)
-    # Find contours
-    contours, _ = cv.findContours(canny_output, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    # Find the convex hull object for each contour
-    hull_list = []
-    for i in range(len(contours)):
-        hull = cv.convexHull(contours[i])
-        hull_list.append(hull)
-    # Draw contours + hull results
-    drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
-    for i in range(len(contours)):
-        color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
-        cv.drawContours(drawing, contours, i, color)
-        cv.drawContours(drawing, hull_list, i, color)
-    # Show in a window
-    cv.imshow('Contours', drawing)
+import matplotlib.pyplot as plt
+from skimage.morphology import convex_hull_image
+from skimage import data, img_as_float
+from skimage.util import invert
+
+
 # Load source image
 parser = argparse.ArgumentParser(description='Code for Convex Hull tutorial.')
 parser.add_argument('--input', help='Path to input image.', default='pc2.JPEG')
@@ -35,14 +20,28 @@ if src is None:
 # Convert image to gray and blur it
 src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
 src_gray = cv.blur(src_gray, (3,3))
-# Create Window
-source_window = 'Source'
-cv.namedWindow(source_window)
-cv.imshow(source_window, src)
-max_thresh = 255
-thresh1 = 100 # initial threshold
-thresh2 = 100
-cv.createTrackbar('Canny thresh1:', source_window, thresh1, max_thresh, thresh_callback)
-#cv.createTrackbar('Canny thresh1:', source_window, thresh2, max_thresh, thresh_callback)
-thresh_callback(thresh1)
-cv.waitKey()
+
+image = cv.Canny(src_gray, 100, 200)
+
+chull = convex_hull_image(image)
+fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+ax = axes.ravel()
+
+ax[0].set_title('Original picture')
+ax[0].imshow(image, cmap=plt.cm.gray)
+ax[0].set_axis_off()
+
+ax[1].set_title('Transformed picture')
+ax[1].imshow(chull, cmap=plt.cm.gray)
+ax[1].set_axis_off()
+
+count = (chull == 1).sum()
+x_center, y_center = np.argwhere(chull == 1).sum(0)/count
+
+plt.tight_layout()
+plt.show()
+
+print("Cord x:", x_center,"Cord y:", y_center)
+
+
+
