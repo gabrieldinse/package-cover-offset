17b26065f813baaa66115683addc1138355f1669
Gabriel Dinse <gabriel_dinse@hotmail.com>
2020-11-20 23:42:09 -0300

    Adicionado funcionalidade para VideoInfoExtractor, template matching e calculo de centroide

--- src/Vision/VideoInfoExtractor.py
+++ src/Vision/VideoInfoExtractor.py
@@ -28,6 +28,8 @@ class VideoInfoExtractor:
         self.max_package_area = 5000000
         self.max_template_value = 0.25
         self.scale_factor = 1
+        self.threshold_position = 0
+        self.template_matching_method = cv2.TM_SQDIFF_NORMED
 
         # TEMPORARIO SUPER
         self.frame = cv2.imread("../Images/pc1.jpg", 1)
@@ -100,26 +102,25 @@ class VideoInfoExtractor:
             print(time.time() - start)
 
     def get_convex_package(self):
-        gray_frame = cv2.cvtColor(self.frame, cv2.COLOR_RGB2GRAY)
+        self.gray_frame = cv2.cvtColor(self.frame, cv2.COLOR_RGB2GRAY)
         blurred_gray_frame = cv2.GaussianBlur(
             gray_frame,
             (self.gaussian_kernel_size, self.gaussian_kernel_size), 0)
 
-        # self.calculate_canny_threshold(blurred_gray_frame)
         canny_edges = cv2.Canny(
             blurred_gray_frame, self.lower_canny, self.upper_canny)
         self.convex_hull = img_as_ubyte(convex_hull_image(canny_edges))
+        contours, _ = cv2.findContours(self.convex_hull, cv2.RETR_TREE,
+                                       cv2.CHAIN_APPROX_SIMPLE)
+        contour = contours[0]
+        self.x, self.y, self.w, self.h = cv2.boundingRect(contour)
         # cv2.imshow("test", convex_hull)
         # if cv2.waitKey(1) & 0xFF == ord('q'):
         #     return
-        contours, _ = cv2.findContours(self.convex_hull, cv2.RETR_TREE,
-                                       cv2.CHAIN_APPROX_SIMPLE)
-        cnt = contours[0]
-        self.x, self.y, self.w, self.h = cv2.boundingRect(cnt)
-        self.convex_hull = self.convex_hull[self.y:self.y+self.h,self.x:self.x+self.w]
-        cv2.imshow("test kkk", gray_frame[self.y:self.y+self.h,self.x:self.x+self.w])
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            return
+        # self.convex_hull = self.convex_hull[self.y:self.y+self.h,self.x:self.x+self.w]
+        # cv2.imshow("test kkk", gray_frame[self.y:self.y+self.h,self.x:self.x+self.w])
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
+        #     return
 
     def calculate_package_centroid(self):
         area = (self.convex_hull == 255).sum()
@@ -128,51 +129,51 @@ class VideoInfoExtractor:
         else:
             self.package_centroid = None
 
-    def package_centroid_is_accepted(self):
-        return (self.package_centroid is not None)
-                # and self.package_centroid >= self.threshold_position)
+    def package_centroid_is_valid(self):
+        return (self.package_centroid is not None
+                and self.package_centroid >= self.threshold_position)
 
     def calculate_cover_centroid(self):
-        pass
-        if self.package_centroid_is_accepted():
-
+        if self.package_centroid_is_valid():
             found = None
-            method = eval('cv2.TM_SQDIFF_NORMED')
             for scale in np.linspace(0.9, 1.1, 5):
                 for orientation in ["0", "180"]:
-                    resized_template = imutils.resize(self.template,
-                                             width=int(self.template.shape[1] * scale))
+                    resized_template = imutils.resize(
+                        self.template, width=int(self.template.shape[1] * scale))
                     h, w = resized_template.shape
                     if orientation == "180":
                         center = (w / 2, h / 2)
-                        M = cv2.getRotationMatrix2D(center, 180, 1.0)
-                        resized_template = cv2.warpAffine(resized_template, M, (h, w))
+                        rotation = cv2.getRotationMatrix2D(center, 180, 1.0)
+                        resized_template = cv2.warpAffine(
+                            resized_template, rotation, (h, w))
 
-                    gray_frame = cv2.cvtColor(self.frame, cv2.COLOR_RGB2GRAY)
-                    res = cv2.matchTemplate(gray_frame[self.y:self.y+self.h,self.x:self.x+self.w], resized_template, method)
-                    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+                    similarity = cv2.matchTemplate(
+                        self.gray_frame[self.y:self.y+self.h,self.x:self.x+self.w],
+                        resized_template, self.template_matching_method)
+                    min_value, _, min_location, _ = cv2.minMaxLoc(similarity)
 
-                    if found is None or min_val < found[0]:
-                        found = (min_val, min_loc, scale, max_loc)
+                    if found is None or min_value < found[0]:
+                        found = (min_value, min_location, scale)
 
             if found[0] < self.max_template_value:
                 h, w = self.template.shape
                 self.cover_centroid = found[1][1] + found[2] * h / 2  # TEMP
-            else:
-                self.cover_centroid = None
-        else:
-            self.cover_centroid = None
+                return
+
+        self.cover_centroid = None
+
+    def cover_centroid_is_valid(self):
+        return self.cover_centroid is not None
 
     def can_calculate_offset(self):
-        return (self.package_centroid is not None
-                and self.cover_centroid is not None)
+        return (self.package_centroid_is_valid() and
+                self.cover_centroid_is_valid())
 
     def calculate_offset(self):
-        if self.package_centroid_is_accepted():
-            has_cover = False if self.cover_centroid is None else True
-            if has_cover:
-                offset = (self.cover_centroid - self.package_centroid) * self.scale_factor
-            else:
-                offset = 0.0
+        if self.can_calculate_offset():
+            offset = (self.cover_centroid - self.package_centroid) \
+                         * self.scale_factor
+        else:
+            offset = 0.0
             print(offset)
-            # self.emit("new_product", ProductInfo(datetime_now_str(), offset, has_cover))
+            # self.emit("new_product", ProductInfo(datetime_now_str(), offset, self.cover_centroid_is_valid()))
